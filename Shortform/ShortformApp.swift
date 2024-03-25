0498a043ba18678178d1bc87828e5026cc655085
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-03-25 23:23:57 +1030

    First Commit: Setup

---
+++ Shortform/ShortformApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortformApp.swift
+//  Shortform
+//
+//  Created by Utsav Budathoki on 25/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShortformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
