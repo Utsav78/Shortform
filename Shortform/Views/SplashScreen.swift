31c13d64687f10548f29eaa8cd04cbfad353f9be
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-03-26 22:52:01 +1030

    Add Splash Screen

---
+++ Shortform/Views/SplashScreen.swift
@@ -0,0 +1,39 @@
+//
+//  SplashScreen.swift
+//  Shortform
+//
+//  Created by Utsav Budathoki on 26/3/2024.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var scale = 0.7
+    @Binding var isActive: Bool
+    var body: some View {
+        VStack {
+            VStack {
+                Image(systemName: "scribble.variable")
+                    .font(.system(size: 100))
+                    .foregroundColor(.blue)
+                Text("Shortform")
+                    .font(.system(size: 20))
+            }.scaleEffect(scale)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 0.7)) {
+                        self.scale = 0.9
+                    }
+                }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreen(isActive: .constant(false))
+}
